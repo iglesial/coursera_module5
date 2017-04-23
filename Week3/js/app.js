d59664ab9160ba0fe6180809a447e35d6340fb42
iglesial <lu.iglesiash@gmail.com>
2017-04-22 18:29:23 -0700

    week3

---
+++ Week3/js/app.js
@@ -0,0 +1,81 @@
+(function () {
+  'use strict';
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems',FoundItemsDirective);
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var narrow = this;
+    console.log(this);
+
+    narrow.now = function (searchTerm){
+                narrow.found = [];
+        if (searchTerm == undefined || searchTerm.length == 0){
+          narrow.found = [];
+          console.log("nothing...")
+        }
+        else{
+          console.log(searchTerm)
+          var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+          promise.then(function(response){
+               var item ={};
+          for (var i = 0; i < response.data.menu_items.length; i++) {   
+               item = response.data.menu_items[i]; 
+                if(response.data.menu_items[i].description.includes(searchTerm)){
+                   narrow.found.push(item);
+                 };
+
+           };
+           console.log(narrow);
+
+          })
+          .catch(function(error){
+            console.log("So close!")
+          })
+
+        }
+        
+
+    };
+
+    narrow.remove = function(index){
+      narrow.found.splice(index,1);
+    }
+    
+  };
+
+
+
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http){
+    var service = this;
+    service.getMatchedMenuItems = function(searchTerm){
+    var response = $http({
+      method: "GET",
+      url: ("https://davids-restaurant.herokuapp.com/menu_items.json"),
+    });
+
+      return response;
+    };
+
+  };
+
+function FoundItemsDirective(){
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope:{
+        foundItems: '<',
+        onRemove: '&'
+    },
+    restrict: 'E'
+  };
+
+  return ddo;
+
+};
+
+
+})();
